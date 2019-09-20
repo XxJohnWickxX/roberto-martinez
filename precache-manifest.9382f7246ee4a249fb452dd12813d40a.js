a7b133dd37032011e887007b16016cae530c683a
Jonathan <tumjoncrm@gmail.com>
2019-09-20 12:45:36 -0500

    Updates

---
+++ precache-manifest.9382f7246ee4a249fb452dd12813d40a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "088b3de6a022745283ff516d8cbb5bd0",
+    "url": "/roberto-martinez/index.html"
+  },
+  {
+    "revision": "d5eb8e8f1d32ea67c829",
+    "url": "/roberto-martinez/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "193483c4426b433356e2",
+    "url": "/roberto-martinez/static/js/2.1df68844.chunk.js"
+  },
+  {
+    "revision": "d5eb8e8f1d32ea67c829",
+    "url": "/roberto-martinez/static/js/main.ba913bc1.chunk.js"
+  },
+  {
+    "revision": "57c8d5a5ad4b2761071b",
+    "url": "/roberto-martinez/static/js/runtime-main.3037b580.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/roberto-martinez/static/media/logo.25bf045c.svg"
+  }
+]);
